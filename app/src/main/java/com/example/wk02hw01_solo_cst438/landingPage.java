77fead1142e989d81a3bd4789013f4ff93c70df3
nathanlatronica <nlatronica@csumb.edu>
2020-09-04 23:02:58 -0700

    landing page

--- app/src/main/java/com/example/wk02hw01_solo_cst438/landingPage.java
+++ app/src/main/java/com/example/wk02hw01_solo_cst438/landingPage.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class landingPage extends AppCompatActivity {
-
-
+    private TextView welcome;
     private static String value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        welcome = findViewById(R.id.welcome);
+
+        welcome.setText("Welcome " + value);
     }
 
     public static Intent getIntent(Context context, String username) {
